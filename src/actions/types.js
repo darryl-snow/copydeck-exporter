8ec2e20754575d2da011b614ebd9624e105bdcc0
Darryl Snow <darryl@yourweb.expert>
2017-09-21 07:33:47 +0800

    replace app with redirect

--- src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const APP_SWITCH = 'app_switch';
-export const GET_CONTENT = 'get_content';
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import AppReducer from './app-reducer';
-
-export default combineReducers({
-  app: AppReducer
-});
